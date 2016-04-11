3dad880652631af2c668f612892c1618e052750b
kevinLiChina <wisdom_li@msn.com>
2016-04-11 15:54:02 +0800

    Update Form1.cs
    Test

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -19,6 +19,7 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //Test code source control
             MessageBox.Show("Hello world");
         }
     }
